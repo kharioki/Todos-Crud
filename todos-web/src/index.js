8ac48cb86737170aee540ec140c0fe7e845af66e
tony <antolisah@gmail.com>
2022-01-29 17:56:41 +0300

    init contract methods

--- todos-web/src/index.js
+++ todos-web/src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import getConfig from './config.js';
+import * as nearAPI from 'near-api-js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Initializing contract
+async function initContract() {
+  const nearConfig = getConfig(process.env.NODE_ENV || 'testnet');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // initialize connection to the NEAR Testnet
+  const near = await nearAPI.connect({
+    deps: {
+      keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
+    },
+    ...nearConfig,
+  });
+
+  // Needed to access wallet
+  const walletConnection = new nearAPI.WalletConnection(near);
+
+  // Load in account data
+  let currentUser;
+  if (walletConnection.getAccountId()) {
+    currentUser = {
+      accountId: walletConnection.getAccountId(),
+      balance: (await walletConnection.account().state()).amount
+    };
+  }
+
+  // initializing our contract APIs by contract name and configuration
+  const contract = await new nearAPI.Contract(
+    walletConnection.account(),
+    nearConfig.contractName,
+    {
+      // view methods are read-only - they don't modify the state, but usually return some value
+      viewMethods: ['get'],
+      // change methods can modify the state, but you don't receive the returned value when called
+      changeMethods: ['create', 'update', 'del'],
+      // sender is the account ID to initialize transactions with
+      // getAccountId() returns the current account ID, returns empty string if user is still unrecognized
+      sender: walletConnection.getAccountId(),
+    });
+
+  return { contract, currentUser, nearConfig, walletConnection };
+}
+
+window.nearInitPromise = initContract()
+  .then(({ contract, currentUser, nearConfig, walletConnection }) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App
+          contract={contract}
+          currentUser={currentUser}
+          nearConfig={nearConfig}
+          wallet={walletConnection}
+        />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
